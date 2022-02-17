92e91b94ba2ed053aa82d1226cd823ea01a4fbac
AlanDeveloper <alanssantos32@gmail.com>
2022-02-17 18:09:41 -0300

    Update table and load data

--- front-end/js/loadData.js
+++ front-end/js/loadData.js
@@ -1,8 +1,29 @@
+function fillTbody(data) {
+	data.forEach((e, index) => {
+		if (index > 4) return;
+		$('tbody').before(`
+			<tr>
+				<td class="text-bold">${e.id}</td>
+				<td>${e.nome}</td>
+				<td>${e.idade}</td>
+			</tr>
+		`);
+	});
+	if (data.length > 5) createBottomBar(data);
+}
+
+function createBottomBar(data) {
+	$('tbody').before(`
+		<tr id="bottomBar">
+			<td class="text-bold" colspan="3">${data.length <= 5 ? 'Existem mais de 5 registros, refine sua pesquisa' : 'Nenhum registro foi encontrado'}</td>
+		</tr>
+	`);
+}
+
 $(document).ready(function () {
-	// $.ajax({
-	// 	url: "http://localhost:3000/people"
-	// }).done(function (res) {
-	// 	console.log(res);
-	// });
-	console.log('oi');
+	$.ajax({
+		url: "http://localhost:3000/people"
+	}).done(function (res) {
+		fillTbody(res);
+	});
 });
